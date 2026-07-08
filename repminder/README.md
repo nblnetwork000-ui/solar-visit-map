4804746287156e4a20b2be54ef8629d42d4d0c59
Yusaku <nblnetwork.000@gmail.com>
2026-07-08 12:45:05 +0900

    Add RepMinder PWA

---
+++ repminder/README.md
@@ -0,0 +1,11 @@
+# RepMinder
+
+Smartphone-first workout reminder PWA.
+
+## Use
+
+Open `index.html` from a web server, then add it to your phone home screen.
+
+## Deploy
+
+This project is ready for GitHub Pages. Serve the repository root.
---
+++ repminder/sw.js
@@ -0,0 +1,52 @@
+const cacheName = "repminder-v25";
+const assets = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/SINEDITOR_1080-1920.png",
+  "./assets/sineditor-live-wallpaper.mp4",
+  "./assets/voice-hello.m4a",
+  "./assets/voice-done.m4a",
+  "./assets/voice-complete.m4a",
+  "./assets/voice-reminder.m4a",
+  "./assets/voice-day-1.m4a",
+  "./assets/voice-day-2.m4a",
+  "./assets/voice-day-3.m4a",
+  "./assets/voice-day-4.m4a",
+  "./assets/voice-day-5.m4a",
+  "./assets/voice-day-6.m4a",
+  "./assets/voice-day-7.m4a",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
